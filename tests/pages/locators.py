4d911885a5fc91f90d1a213513e9bad6da63f6e4
eldar <dr.shamhalov@mail.ru>
2019-11-07 15:54:13 +0300

    Увольнение,сохранеие файлов

---
+++ tests/pages/locators.py
@@ -0,0 +1,16 @@
+from selenium.webdriver.common.by import By
+
+
+class MainPageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+
+
+class LoginPageLocators():
+    LOGIN_EMAIL = (By.CSS_SELECTOR, "#id_login-username")
+    LOGIN_PASSWORD = (By.CSS_SELECTOR, "#id_login-password")
+    LOGIN_BUTTON = (By.CSS_SELECTOR, 'button[name = "login_submit"]')
+
+    REGISTER_EMAIL = (By.CSS_SELECTOR, "#id_registration-email")
+    REGISTER_PASSWORD = (By.CSS_SELECTOR, "#id_registration-password1")
+    REGISTER_CONFIRM_PASSWORD = (By.CSS_SELECTOR, "#id_registration-password2")
+    REGISTER_BUTTON = (By.CSS_SELECTOR, 'button[name = "registration_submit"]')
---
+++ tests/test_main_page.py
@@ -0,0 +1,22 @@
+from tests.pages.login_page import LoginPage
+from tests.pages.main_page import MainPage
+
+
+def test_guest_can_go_to_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+
+
+def test_guest_should_see_login_link(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+def test_login_and_register_form(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_login_form()
